8d936eeeafec5492f8b1b42935b6310ec03ff7fb
Vimal Ramnarain <vimalramnarain@gmail.com>
2025-04-26 05:27:42 -0400

    First Commit

---
+++ app/src/main/java/com/vimal/viewsbasic/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.vimal.viewsbasic
+
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+/* Views
+   Simple building block of a user interface.
+
+   Views are small rectangular boxes that can
+   be TextView, EditText, or even a button.
+
+   They occupy the area on the screen in a
+   rectangular area and are responsible for
+   drawing and event handling.
+ */
+
+/* ViewGroup
+   A special subclass of the View Class that
+   serves as a container for multiple Views.
+
+   There are several useful widgets that extend
+   the ViewGroup Class, such as LinearLayout.
+
+   Most are actually subclasses of another
+   ViewGroup known as Layout.
+ */
+
+class MainActivity : AppCompatActivity() {
+    lateinit var myTextView: TextView
+    lateinit var myEditText: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        // Set the content view of an activity to a specified layout.
+        setContentView(R.layout.activity_main)
+
+        // Initialize Views
+        myTextView = findViewById(R.id.textView1)
+        myTextView.setText("Hi there!")
+
+        myEditText = findViewById(R.id.editText1)
+        var myText = myEditText.text.toString()
+
+    }
+}
+
